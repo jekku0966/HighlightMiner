59baefc362070e270b5394fba0012c71ca99d2ec
NKS <niklas.jerva@gmail.com>
2026-08-17 20:59:20 +0300

    Test settings UI modules import cleanly

---
+++ tests/test_ui_settings_imports.py
@@ -0,0 +1,6 @@
+def test_settings_ui_modules_import() -> None:
+    import highlightminer.app  # noqa: F401
+    import highlightminer.settings_store  # noqa: F401
+    import highlightminer.ui_common  # noqa: F401
+    import highlightminer.ui_mine  # noqa: F401
+    import highlightminer.ui_settings  # noqa: F401
